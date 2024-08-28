dcda79a6d469780acc6cc9b169426b82bdc36978
Berk Birkan <berk-birkan@hotmail.com>
2024-08-28 18:20:28 +0300

    first commit

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
-# setup.py
-
 from setuptools import setup, find_packages
 
 setup(
-    name='irrelevant-content-detection',
-    version='0.1',
-    description='A Python package for detecting irrelevant content in texts.',
-    author='Berk Birkan',
-    author_email='info@berkbirkan.com',
+    name='irrelevant-content-detection',  # Paket adınız
+    version='0.1',  # Versiyon numarası
+    description='A Python package for detecting irrelevant content in text and HTML.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Berk Birkan',  # İsminiz
+    author_email='info@berkbirkan.com',  # E-posta adresiniz
+    url='https://github.com/berkbirkan/irrelevant-content-detection',  # Proje URL'si
     packages=find_packages(),
     install_requires=[
         'numpy',
         'scikit-learn',
-        'scipy'
+        'scipy',
+        'beautifulsoup4'
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.6',  # Minimum Python sürümü gereksinimi
 )
